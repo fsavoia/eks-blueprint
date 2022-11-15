f58946d86d412e4d75b765a9fd0b0965cbcc6780
Felipe Savoia <fsavoia@amazon.com>
2022-11-15 01:46:43 -0300

    fix sintaxe

--- versions.tf
+++ versions.tf
@@ -2,27 +2,27 @@ provider "aws" {
   region = local.region
 }
 
-# provider "kubernetes" {
-#   host                   = module.eks_blueprints.eks_cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.this.token
-# }
+provider "kubernetes" {
+  host                   = module.eks_blueprints.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.eks_blueprints.eks_cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-#     token                  = data.aws_eks_cluster_auth.this.token
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_blueprints.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
 
-# provider "kubectl" {
-#   apply_retry_count      = 10
-#   host                   = module.eks_blueprints.eks_cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-#   load_config_file       = false
-#   token                  = data.aws_eks_cluster_auth.this.token
-# }
+provider "kubectl" {
+  apply_retry_count      = 10
+  host                   = module.eks_blueprints.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.this.token
+}
 
 terraform {
   required_version = ">= 1.0.0"
@@ -32,10 +32,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.9"
     }
-    # kubernetes = {
-    #   source  = "hashicorp/kubernetes"
-    #   version = ">= 2.10"
-    # }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.4.1"
